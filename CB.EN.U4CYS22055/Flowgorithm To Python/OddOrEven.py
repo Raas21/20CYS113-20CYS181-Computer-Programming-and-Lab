76800f6065ad9aecc7bf01ab42cc23eb119ada57
Raas21 <116824481+Raas21@users.noreply.github.com>
2023-04-01 22:42:40 +0530

    CB.EN.U4CYS22055

---
+++ CB.EN.U4CYS22055/Flowgorithm To Python/OddOrEven.py
@@ -0,0 +1,10 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("PRINTING CELCIUS IN FAHRENHEIT")
+... 
+... print("enter your num")
+... num = int(input())
+... if num % 2 == 0:
+...     print("The num is even")
+... else:
+...     print("The num is odd")
